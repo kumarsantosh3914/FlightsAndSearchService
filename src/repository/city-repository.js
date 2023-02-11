01b14633d9de575e26906523d5c5677bcf7e52a1
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-02-11 22:47:48 +0530

    Added city service

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,3 +1,5 @@
+const { Op } = require('sequelize');
+
 const { City } = require('../models/index');
 
 class CityRepository {
@@ -49,6 +51,26 @@ class CityRepository {
         try {
             const city = await City.findByPk(CityId);
             return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
+        }
+    }
+
+    async getAllCities(filter) {
+        try {
+            if (filter.name) {
+                const cities = await City.findAll({
+                    where: {
+                        name: {
+                            [Op.startsWith]: filter.name
+                        }
+                    }
+                });
+                return cities;
+            }
+            const cities = await City.findAll();
+            return cities;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
             throw { error };
