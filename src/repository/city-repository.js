d61bb89447bb772eb658b7af41eb60635b2e225a
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-02-11 22:15:02 +0530

    Completed city repository

---
+++ src/repository/city-repository.js
@@ -0,0 +1,59 @@
+const { City } = require('../models/index');
+
+class CityRepository {
+    async createCity({ name }) {
+        try {
+            const city = await City.create({ name });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
+        }
+    }
+
+    async deleteCity({ CityId }) {
+        try {
+            await City.destroy({
+                where: {
+                    id: CityId
+                }
+            });
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error }
+        }
+    }
+
+    async updateCity(CityId, data) {
+        try {
+            // The below approach also works but will not return updated object
+            // if we are using Pg then returning: true can be used, else not
+            // const city = await City.update(data, {
+            //     where: {
+            //         id: cityId
+            //     },
+            //      
+            // });
+            // for getting updated data in mysql we use the below approach
+            const city = await City.findByPk(CityId);
+            city.name = data.name;
+            await city.save();
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
+        }
+    }
+
+    async getCity(CityId) {
+        try {
+            const city = await City.findByPk(CityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
+        }
+    }
+}
+
+module.exports = CityRepository;
