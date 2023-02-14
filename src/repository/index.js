638cbaed761abc1cee3f9a6499c7bc1a703b1994
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-02-14 16:50:00 +0530

    Added common crud classes for basic crud operations

--- src/repository/index.js
+++ src/repository/index.js
@@ -2,4 +2,6 @@ module.exports = {
     CityRepository: require('./city-repository'),
     FlightRepository: require('./flight-repository'),
     AirplaneRepository: require('./airplane-repository'),
+    AirportRespository: require('./airport-respository'),
+    CrudRespository: require('./crud-repository'),
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const CityController = require('../../controllers/city-controller');
 const FlightController = require('../../controllers/flight-controller');
+const AirportController = require('../../controllers/airport-controller');
 
 const router = express.Router();
 
@@ -10,9 +11,11 @@ router.get('/city/:id', CityController.get);
 router.get('/city', CityController.getAll);
 router.patch('/city/:id', CityController.update);
 
-router.post('/flights', FlightController.create);
-router.get('/flights', FlightController.getAll);
-router.get('/flights/:id', FlightController.get);
-router.patch('/flights/:id', FlightController.update);
+// router.post('/flights', FlightController.create);
+// router.get('/flights', FlightController.getAll);
+// router.get('/flights/:id', FlightController.get);
+// router.patch('/flights/:id', FlightController.update);
+
+router.post('/airports', AirportController.create);
 
 module.exports = router;
